9198d06c8674344dbb71807cbb12f2f82ea4b21c
IBRAHIM\Ibrahin <ishaheen910@gmail.com>
2022-02-09 17:34:50 +0200

    edit to action.type

--- src/Redux/reducers/users.js
+++ src/Redux/reducers/users.js
@@ -3,11 +3,14 @@ import * as type from "../types";
 const initialState = {
   users: [],
 };
-export default function users(state = initialState, action) {
-  switch (action) {
-    case type.GET_USERS:
-      return { ...state, users: action.payload };
 
+export default function users(state = initialState, action) {
+  switch (action.type) {
+    case type.GET_USERS:
+      return {
+        ...state,
+        users: action.payload,
+      };
     default:
       return state;
   }
