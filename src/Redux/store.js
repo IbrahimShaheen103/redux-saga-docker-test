2278d7716f4e7ed8cdf61f824c0f7aba8f5ef61d
IBRAHIM\Ibrahin <ishaheen910@gmail.com>
2022-02-09 18:11:43 +0200

    connect store with saga

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,5 +1,12 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
+import createSagaMiddleware from "redux-saga";
 import rootReducer from "./reducers";
+import rootSaga from "./sagas";
+const sagaMiddleware = createSagaMiddleware();
+const store = compose(
+  applyMiddleware(sagaMiddleware),
+  window.devToolsExtension && window.devToolsExtension()
+)(createStore)(rootReducer);
 
-const store = createStore(rootReducer);
+sagaMiddleware.run(rootSaga);
 export default store;
