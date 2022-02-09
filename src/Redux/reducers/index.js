9628185e310f9fccc676d1c2fb0abb5d381ce088
IBRAHIM\Ibrahin <ishaheen910@gmail.com>
2022-02-09 16:38:57 +0200

    add user action ad reducer

--- src/Redux/reducers/index.js
+++ src/Redux/reducers/index.js
@@ -1,5 +1,7 @@
 import { combineReducers } from "redux";
-
-const rootReducer = combineReducers({});
+import users from "./users";
+const rootReducer = combineReducers({
+  users: users,
+});
 
 export default rootReducer;
